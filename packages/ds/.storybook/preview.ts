13720da1a44c42a14e96b3e5a8e4cfcc410df7df
Julie Muzina <julie.muzina@canonical.com>
2024-10-01 14:06:48 -0400

    Add theme switcher to storybook

--- packages/ds/.storybook/preview.ts
+++ packages/ds/.storybook/preview.ts
@@ -1,4 +1,5 @@
-import type { Preview } from "@storybook/react";
+import { withThemeByClassName } from "@storybook/addon-themes";
+import type { Preview, ReactRenderer } from "@storybook/react";
 
 const preview: Preview = {
   parameters: {
@@ -9,6 +10,16 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [
+    withThemeByClassName<ReactRenderer>({
+      themes: {
+        light: "is-light",
+        dark: "is-dark",
+        paper: "is-paper",
+      },
+      defaultTheme: "light",
+    }),
+  ],
 };
 
 export default preview;
