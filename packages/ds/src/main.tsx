9aaa2b781f2ad62280fd747e0218c1a9bee68d3a
Julie Muzina <julie.muzina@canonical.com>
2024-10-08 11:26:39 -0400

    Delete main.tsx and remove dev npm command

--- packages/ds/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-
-createRoot(document.getElementById("root") as HTMLElement).render(
-  <StrictMode>
-
-  </StrictMode>,
-);
