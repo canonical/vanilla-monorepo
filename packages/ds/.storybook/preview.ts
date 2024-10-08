045ad27810acbea8e436438722f2593b43a915bf
Julie Muzina <julie.muzina@canonical.com>
2024-10-08 10:50:06 -0400

    feat: Base storybook (#7)
    * Storybook init
    
    * Disable storybook telemetry
    
    * Removed unnecessary commands
    
    * Storybook run
    
    * Base linting and ts config
    
    * Type check is passing
    
    * -
    
    * Add theme switcher to storybook
    
    * Apply biome linting/formatting fixes
    
    * Expose `src/assets/` storybook files at `/assets`
    
    * React TS config - allow importing with TS extensions
    
    * Storybook lint fixes
    
    * Enable typescript check on storybook config
    
    * ignore storybook-static directory (it is a build artifact)
    
    * Remove eslint
    
    * Remove allowing import ts extensions, simplify base tsconfig extension
    
    * change static asset dirs
    
    ---------
    
    Co-authored-by: advl <700@779.mx>

---
+++ packages/ds/.storybook/preview.ts
@@ -0,0 +1,25 @@
+import { withThemeByClassName } from "@storybook/addon-themes";
+import type { Preview, ReactRenderer } from "@storybook/react";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    withThemeByClassName<ReactRenderer>({
+      themes: {
+        light: "is-light",
+        dark: "is-dark",
+        paper: "is-paper",
+      },
+      defaultTheme: "light",
+    }),
+  ],
+};
+
+export default preview;
