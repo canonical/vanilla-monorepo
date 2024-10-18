356cb4adf384295a5771a48873e8a05df4e4e23b
Julie Muzina <julie.muzina@canonical.com>
2024-10-18 14:47:32 -0400

    Add a bit of company personalization to the boilerplate

--- apps/boilerplate-react-vite/src/App.tsx
+++ apps/boilerplate-react-vite/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import canonicalLogo from '/canonical.svg';
 import './App.css'
 
 import { Button } from '@canonical/ds';
@@ -11,14 +11,15 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://canonical.com" target="_blank" referrerPolicy='no-referrer'>
+          <img src={canonicalLogo} className="logo" alt="Canonical logo"></img>
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Canonical Design System</h1>
+      <h2>React Vite template</h2>
       <div className="card">
         <Button 
           label={`Count: ${count}`}
@@ -29,9 +30,6 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
