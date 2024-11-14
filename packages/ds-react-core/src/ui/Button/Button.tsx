3e9b6240ea606b9c6412b440d773204ab84344de
advl <700@779.mx>
2024-11-14 10:54:44 +0100

    Minor proposals on props/css

--- packages/ds-react-core/src/ui/Button/Button.tsx
+++ packages/ds-react-core/src/ui/Button/Button.tsx
@@ -2,19 +2,45 @@ import type React from "react";
 
 import "./button.css";
 
+//const baseClassName = 'ds'
+
+export enum ButtonTypeEnum {
+  Button = "button",
+  Submit = "submit"
+}
+
 export interface ButtonProps {
+  id:string,
+  style:any,
+  className:string,
   /** The visual style of the button */
   appearance?: "default" | "base" | "positive" | "negative" | "link";
   /** Button contents */
   label?: string;
   /** Optional click handler */
   onClick?: () => void;
+
+  type:ButtonTypeEnum;
 }
+
+
+/*
+ variant / modifier / appearance
+ when there is a list of children : items (?) <Navigation items=>
+ <Accordion items=>
+ flags : adjective/participate (disabled) or fully boolean isDisabled
+
+ */
 
 /** Primary UI component for user interaction */
 const Button = ({
+  id,
+  className,
+  style,
   appearance = "default",
   label,
+  type:buttonType = ButtonTypeEnum.Button,
+  // ref,
   ...props
 }: ButtonProps): React.ReactElement => {
   return (
