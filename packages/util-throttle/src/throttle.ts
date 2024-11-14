274d9c1e121aed6911003d18a8315a815561e797
Julie Muzina <julie.muzina@canonical.com>
2024-11-14 17:53:20 -0500

    simplify throttle fn

--- packages/util-throttle/src/throttle.ts
+++ packages/util-throttle/src/throttle.ts
@@ -1,34 +1,20 @@
 /**
  * Throttles a function invocation to at most once every `wait` milliseconds.
+ * @template T The type of the function to throttle
  * @param func - The function to throttle
  * @param wait - The time in milliseconds to wait between invocations
- * @returns
+ * @returns A throttled version of the function
+ * @see throttling function calls, by Remy Sharp
+ *  http://remysharp.com/2010/07/21/throttling-function-calls/
  */
 export default function throttle<
   T extends (...args: Parameters<T>) => ReturnType<T>,
 >(func: T, wait: number): (...args: Parameters<T>) => void {
-  let lastCallTime: number | null = null;
-  let timeout: ReturnType<typeof setTimeout> | null = null;
-
-  return (...args: Parameters<T>) => {
-    const now = Date.now();
-
-    if (lastCallTime === null || now - lastCallTime >= wait) {
-      // If the time since the last call is more than the wait time, invoke the function
-      if (timeout) {
-        clearTimeout(timeout);
-        timeout = null;
-      }
-      lastCallTime = now;
-      func(...args);
-    } else if (!timeout) {
-      // Otherwise, set a timeout to invoke the function after the remaining time
-      const remainingTime = wait - (now - lastCallTime);
-      timeout = setTimeout(() => {
-        lastCallTime = Date.now();
-        timeout = null;
-        func(...args);
-      }, remainingTime);
-    }
+  let timer: NodeJS.Timeout | null = null;
+  return function (this: unknown, ...args: Parameters<T>) {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, wait);
   };
 }
