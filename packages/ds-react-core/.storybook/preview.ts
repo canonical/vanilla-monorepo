578795048a873e19b659c72a60da3cb70f175d61
Bartek Szopka <bartek.szopka@canonical.com>
2024-11-19 17:24:30 +0100

    Allow vite to apply paths from tsconfig

--- packages/ds-react-core/.storybook/preview.ts
+++ packages/ds-react-core/.storybook/preview.ts
@@ -1,7 +1,7 @@
 import { withThemeByClassName } from "@storybook/addon-themes";
 import type { Preview, ReactRenderer } from "@storybook/react";
 
-import "../src/index.css";
+import "index.css";
 
 const preview: Preview = {
   parameters: {
