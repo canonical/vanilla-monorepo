e93a5a47524b1a077b6af99bac6bd8d41a87f997
Julie Muzina <julie.muzina@canonical.com>
2024-11-20 13:34:23 -0500

    Simplify boilerplate tsconfigs

--- apps/boilerplate-react-vite/vite.config.ts
+++ apps/boilerplate-react-vite/vite.config.ts
@@ -4,10 +4,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    tsconfigPaths({
-      projects: ["tsconfig.app.json"],
-    }),
-    react(),
-  ],
+  plugins: [tsconfigPaths(), react()],
 });
