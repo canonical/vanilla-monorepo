ee635c2297f525d8f4c0fd981f95fcb7bd27a472
advl <700@779.mx>
2024-12-12 17:22:30 +0100

    PoC for sprite usage with svg/use

---
+++ packages/ds-react-core/src/ui/UseSVG/UseSVG.tsx
@@ -0,0 +1,34 @@
+/* @canonical/generator-canonical-ds 0.4.0-experimental.0 */
+import type React from "react";
+import type { UseSVGProps } from "./types.js";
+
+/**
+ * description of the UseSVG component
+ * @returns {React.ReactElement} - Rendered UseSVG
+ */
+const UseSVG = ({
+  id,
+  className,
+  style,
+  spritePathname = "/sprite.svg",
+  spriteTarget,
+  // do....
+}: UseSVGProps): React.ReactElement => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      version="1.1"
+      width="512"
+      height="512"
+      viewBox="0 0 16 16"
+      className={[className].filter(Boolean).join(" ")}
+      id={id}
+      style={style}
+    >
+      <title>{spriteTarget}</title>
+      <use href={`${spritePathname}#${spriteTarget}`} />
+    </svg>
+  );
+};
+
+export default UseSVG;
