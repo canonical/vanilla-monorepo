731945577644106bac846c3dd1336293629610e1
advl <700@779.mx>
2024-12-13 16:55:23 +0100

    Added the base Renderer and Extractor

--- packages/react-ssr/src/renderers/Extractor.ts
+++ packages/react-ssr/src/renderers/Extractor.ts
@@ -1,3 +1,62 @@
-const a = 1;
+import { parseDocument } from "htmlparser2";
+import type { Element } from "domhandler";
+import React from "react";
 
-export default a;
+/**
+ * Parses an HTML string to extract and convert script and link tags to React.createElement calls.
+ */
+class Extractor {
+  private document: any;
+
+  constructor(html: string) {
+    this.document = parseDocument(html);
+  }
+
+  private getElementsByTagName(tagName: string): Element[] {
+    const elements: Element[] = [];
+    const stack = [this.document];
+
+    while (stack.length) {
+      const node = stack.pop();
+      if (!node) continue;
+
+      if (node.type === "tag" && node.name === tagName) {
+        elements.push(node);
+      }
+
+      // Check for script tags specifically
+      if (node.type === "script" && tagName === "script") {
+        elements.push(node);
+      }
+
+      if (node.children) {
+        stack.push(...node.children);
+      }
+    }
+
+    console.log(`Found ${elements.length} <${tagName}> tags`);
+    return elements;
+  }
+
+  private convertToReactElement(element: Element): React.ReactElement {
+    const props: { [key: string]: string } = {};
+
+    for (const [key, value] of Object.entries(element.attribs)) {
+      props[key] = value;
+    }
+
+    return React.createElement(element.name, props);
+  }
+
+  public getLinkTags(): React.ReactElement[] {
+    const linkElements = this.getElementsByTagName("link");
+    return linkElements.map(this.convertToReactElement);
+  }
+
+  public getScriptTags(): React.ReactElement[] {
+    const scriptElements = this.getElementsByTagName("script");
+    return scriptElements.map(this.convertToReactElement);
+  }
+}
+
+export default Extractor;
--- packages/react-ssr/src/renderers/JSXRenderer.ts
+++ packages/react-ssr/src/renderers/JSXRenderer.ts
@@ -1,3 +1,52 @@
-const a = 1;
+import * as React from "react";
+import { createElement } from "react";
+import { renderToReadableStream } from "react-dom/server";
+import Extractor from "./Extractor.js";
 
-export default a;
+interface RendererOptions {
+  defaultLocale?: string;
+  loadMessages?: (locale: string) => Promise<any>;
+  htmlString?: string;
+}
+
+// This class is responsible for rendering a React component to a readable stream.
+export default class Renderer {
+  private Component: React.ComponentType<any>;
+  private options: RendererOptions;
+  private locale: string | undefined;
+  private messages: any;
+  private extractor: Extractor | undefined;
+
+  constructor(
+    Component: React.ComponentType<any>,
+    options: RendererOptions = {},
+  ) {
+    this.Component = Component;
+    this.options = options;
+    this.prepareLocale = this.prepareLocale.bind(this);
+    this.render = this.render.bind(this);
+    this.extractor = this.options.htmlString
+      ? new Extractor(this.options.htmlString)
+      : undefined;
+  }
+
+  async prepareLocale(header: string | undefined) {
+    if (this.options.loadMessages) {
+      this.locale = header
+        ? header.slice(0, 2)
+        : this.options.defaultLocale || "en";
+      //this.messages = await this.options.loadMessages(this.locale);
+    }
+  }
+
+  async render(req: Request) {
+    await this.prepareLocale(req.headers.get("accept-language") || undefined);
+    const jsx = createElement(this.Component, {
+      locale: this.locale,
+      scriptTags: this.extractor?.getScriptTags(),
+      linkTags: this.extractor?.getLinkTags(),
+      //messages: this.messages,
+    });
+    return renderToReadableStream(jsx);
+  }
+}
