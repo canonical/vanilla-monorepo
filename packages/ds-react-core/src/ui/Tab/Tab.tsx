77e59132727a8ae3771404cd1a3154dfea6e7c27
Goulin Khoge <36013798+goulinkh@users.noreply.github.com>
2024-12-16 12:59:40 +0100

    WIP tabs component

--- packages/ds-react-core/src/ui/Tab/Tab.tsx
+++ packages/ds-react-core/src/ui/Tab/Tab.tsx
@@ -2,6 +2,7 @@
 import type React from "react";
 import "./Tab.css";
 import type { TabPropsType } from "./types.js";
+import { forwardRef } from "react";
 
 const componentCssClassName = "ds tab";
 
@@ -9,24 +10,24 @@ const componentCssClassName = "ds tab";
  * description of the Tab component
  * @returns {React.ReactElement} - Rendered Tab
  */
-const Tab = ({
-  selected,
-  id,
-  className,
-  style,
-  ...props
-}: TabPropsType): React.ReactElement => {
-  return (
-    <button
-      id={id}
-      style={style}
-      className={[componentCssClassName, selected && "selected", className]
-        .filter(Boolean)
-        .join(" ")}
-      aria-selected={selected}
-      {...props}
-    />
-  );
-};
+const Tab = forwardRef(
+  (
+    { selected, id, className, style, ...props }: TabPropsType,
+    ref: React.Ref<HTMLButtonElement>
+  ): React.ReactElement => {
+    return (
+      <button
+        ref={ref}
+        id={id}
+        style={style}
+        className={[componentCssClassName, selected && "selected", className]
+          .filter(Boolean)
+          .join(" ")}
+        aria-selected={selected}
+        {...props}
+      />
+    );
+  }
+);
 
 export default Tab;
