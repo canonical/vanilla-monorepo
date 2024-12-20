716f57e60765231c18171187e172bcc255fa9108
Julie Muzina <julie.muzina@canonical.com>
2024-12-20 15:14:48 -0500

    Add vitest setupfiles comment

--- packages/ds-react-core/vitest.config.ts
+++ packages/ds-react-core/vitest.config.ts
@@ -14,6 +14,8 @@ export default mergeConfig(
       environment: "jsdom",
       // include vite globals for terser test code
       globals: true,
+      // Defines files that perform extra vitest configuration
+      // Currently, this is used to extend vitest matchers and cleanup the DOM after each test
       setupFiles: ["./vitest.setup.ts"],
       include: ["src/**/*.test.ts", "src/**/*.test.tsx"],
     },
